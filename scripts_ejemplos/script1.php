84e27493cc3eeae53774689b7057e9286c973177
David sotelo <davidesteban9702@gmail.com>
2019-03-15 11:59:51 -0500

    PHP

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,19 @@
+<center> Programacion avanzada </center>
+<?php
+
+
+echo "BIENVENIDOS";
+$nombre ="Peter";
+$apellido ="Watson";
+$edad ="30";
+$salario ="800";
+
+echo "<center>BIENVENIDOS</center>";
+echo "Nombre: ". $nombre;
+echo "Apellido". $apellido;
+echo "Edad: ". $edad;
+
+
+
+ ?>
+<center>Fin del programa </center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,14 @@
+<?php
+$num1=10;
+$num2 =5;
+$resta= $num1-$num2;
+$mult= $num1*$num2;
+$div = $num1/$num2;
+
+echo "La suma es: ". ($num1+$num2);
+echo "<br>la resta es: ". $resta;
+echo "<br>La multipli. es: ". $mult;
+echo "<br>La división es: ". $div;
+echo "</center>";
+
+ ?>
