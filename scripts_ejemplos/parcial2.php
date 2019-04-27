53a47ab3e20b723c4360637d422f54db853b1ccb
David sotelo <davidesteban9702@gmail.com>
2019-04-26 23:19:53 -0500

    parical2

--- scripts_ejemplos/parcial2.php
+++ scripts_ejemplos/parcial2.php
@@ -9,7 +9,7 @@
     <br></table></br>
     <center><input type="submit" value="Procesar"></center>
 </form>
-<img src="icons/mon.png" width="500" height="100" >
+<img src="icons/mon1.png" width="500" height="100" >
 </body>
 </html>
 
@@ -19,56 +19,27 @@
 
 <?php
           $din=$_POST['cantidad'];
-          echo "<p>El cambio de la cantidad ".$din." es:</p>";
-
-                  // indicamos todas las monedas posibles
+          echo "<p>El cambio de la cantidad <b>".$din."</b> es:</p>";
 
           $monedas=array(100, 50, 20, 10, 5,1);
-
-          // creamos un array con la misma cantidad de monedas
-
-          // Este array contendra las monedas a devolver
-
           $cambio=array(0,0,0,0,0,0);
 
-          // Recorremos todas las monedas
-
           for($i=0; $i<count($monedas); $i++)
-
           {
-
-              // Si el importe actual, es superior a la moneda
-
               if($din>=$monedas[$i])
-
               {
-
-                  // obtenemos cantidad de monedas
-
                   $cambio[$i]=floor($din/$monedas[$i]);
-
-                  // actualizamos el valor del importe que nos queda por didivir
-
                   $din=$din-($cambio[$i]*$monedas[$i]);
-
               }
-
           }
 
-          // Bucle para mostrar el resultado
-
           for($i=0; $i<count($monedas); $i++)
-
           {
-
               if($cambio[$i]>0)
-
               {
-
                   if($monedas[$i]>=5)
 
                       echo "Hay: ".$cambio[$i]." Monedas de <b>".$monedas[$i]."</b><br>";
-
                   else
 
                       echo "Hay: ".$cambio[$i]." Monedas de ".$monedas[$i]."<br>";
